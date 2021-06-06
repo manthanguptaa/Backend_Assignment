83d292932ee145b86b596bca9ec845851ea3c3d2
Manthan Gupta <manthan.gupta@indwealth.in>
2021-06-06 14:54:55 +0530

    Book model created

--- ebook_portal/app/models.py
+++ ebook_portal/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class book(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_id = models.PositiveIntegerField(primary_key=True, auto_created=True)
+    book_name = models.CharField(max_length=50)
+    language = models.CharField(max_length=20)
+    publish_date = models.DateTimeField()
+    summary = models.CharField(max_length=200)
+    content = models.CharField(max_length=1000)
