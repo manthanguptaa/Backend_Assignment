35dfdf7c2f685e945dcc152f2c8bc03153ae0bb2
Manthan Gupta <manthan.gupta@indwealth.in>
2021-06-08 00:11:46 +0530

    Adding statuc url

--- ebook_portal/ebook_portal/settings.py
+++ ebook_portal/ebook_portal/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
